94398bd28e4f41a730afdb3181d87d7140aaef70
Ibrahim Mattar <ibmmtr@gmail.com>
2021-06-29 21:55:20 +0300

    chal 6 done

--- Algorithms/06-sum-array/sum-array.js
+++ Algorithms/06-sum-array/sum-array.js
@@ -1,3 +1,17 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+var sumArray = function (arr) {
+  let totall = arr.reduce((a, b) => a + b, 0);
+  return totall;
+};
+
+sumArray([4, 8, 15, 16, 23, 42]);
+
+// ! Another Solution
+/*
+var sumArray = function (arr) {
+  console.log(arr.reduce((a, b) => a + b, 0));
+};
+
+sumArray([1, 2, 3, 4]);
+*/
