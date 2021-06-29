9581525996c8acec7cd6f844fcdd2c5abf44f45d
Ibrahim Mattar <ibmmtr@gmail.com>
2021-06-29 21:28:36 +0300

    challenge 1 - 4

--- Algorithms/01-arithmetic/arithmetic.js
+++ Algorithms/01-arithmetic/arithmetic.js
@@ -1,9 +1,21 @@
 // Fill in the body of each function with the code required
 
-var add = function(num1, num2) {};
+var add = function (num1, num2) {
+  sumNum = num1 + num2;
+  return sumNum;
+};
 
-var subtract = function(num1, num2) {};
+var subtract = function (num1, num2) {
+  subNum = num1 - num2;
+  return subNum;
+};
 
-var multiply = function(num1, num2) {};
+var multiply = function (num1, num2) {
+  multiNum = num1 * num2;
+  return multiNum;
+};
 
-var divide = function(num1, num2) {};
+var divide = function (num1, num2) {
+  divNum = num1 / num2;
+  return divNum;
+};
--- Algorithms/03-log-nums/log-nums.js
+++ Algorithms/03-log-nums/log-nums.js
@@ -1,4 +1,12 @@
 // Write code to print all numbers from 1 to `num`
 // Assume `num` will be a positive number
 
-var logNums = function(num) {};
+var logNums = function (num) {
+  for (let i = 1; i <= num; i++) {
+    console.log(i);
+  }
+};
+
+// *For test purpose
+//console.log(logNums(5));
+logNums(5);
