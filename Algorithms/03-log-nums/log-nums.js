a5e98dd5e5ad7cdf90da437e7b50cc710a1d4741
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2021-07-02 21:15:50 +0100

    All challenges complete

--- Algorithms/03-log-nums/log-nums.js
+++ Algorithms/03-log-nums/log-nums.js
@@ -1,4 +1,9 @@
 // Write code to print all numbers from 1 to `num`
 // Assume `num` will be a positive number
 
-var logNums = function(num) {};
+let logNums = function (num) {
+  let highestNumber = num;
+  for (let i = 1; i < highestNumber + 1; i++) {
+    console.log(i);
+  }
+};
