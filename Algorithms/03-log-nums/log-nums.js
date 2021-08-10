c6923481d6d38bced911632c926deb5f85829133
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2021-08-10 22:59:26 +0100

    finished Algorithms 02, 03 and 04

--- Algorithms/03-log-nums/log-nums.js
+++ Algorithms/03-log-nums/log-nums.js
@@ -1,4 +1,11 @@
 // Write code to print all numbers from 1 to `num`
 // Assume `num` will be a positive number
 
-var logNums = function(num) {};
+var logNums = function(num) {
+    let count = 1;
+    while(num > 0){
+        console.log(count);
+        num -= 1;
+        count += 1;
+    }
+};
--- Algorithms/04-log-even-nums/log-even-nums.js
+++ Algorithms/04-log-even-nums/log-even-nums.js
@@ -1,4 +1,13 @@
 // Write code to print all even numbers from 0 to `num`
 // Assume `num` will be a positive number
 
-var logEvenNums = function(num) {};
+var logEvenNums = function(num) {
+    //I hope you like my dodgy code dear reader ;)
+    let count = 0;
+    while(count < (num+=0.1)){
+        console.log(count);
+        count += 2;
+    }
+};
+
+console.log(logEvenNums(6));
