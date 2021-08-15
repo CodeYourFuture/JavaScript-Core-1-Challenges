580e3722a5667b27ef21c592e516102b0edd760c
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2021-08-15 20:05:39 +0100

    Finished algorithms 08-max-num

--- Algorithms/08-max-num/max-num.js
+++ Algorithms/08-max-num/max-num.js
@@ -1,3 +1,16 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function(arr) {
+    //declare the largest number to start smaller than
+    //whatever number is first so that it will log fairly.
+    let largestNum = arr[0] - 1;
+    //loop the array all the way through and log the index
+    //if it is larger than the already logged index.
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] > largestNum){
+            largestNum = arr[i]
+        }
+    }
+    //return the largest number.
+    return largestNum
+};
