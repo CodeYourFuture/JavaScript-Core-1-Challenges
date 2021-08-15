5ee1a773e108b821701884504a996ecaed34d288
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2021-08-15 20:31:45 +0100

    Finished algorithms 09-vowel-count

--- Algorithms/09-vowel-count/vowel-count.js
+++ Algorithms/09-vowel-count/vowel-count.js
@@ -1,3 +1,21 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function(str) {
+    //this function counts all the vowels in a string
+    //regardless of casing or spacing :P
+
+    //a list of vowels in bot lower and upper case forms
+    const vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"]
+    //split the word into characters
+    const wordArray = str.split('');
+    let vowelCount = 0;
+    //loops the word array and adds 1 to count whenever it 
+    //finds a vowel.
+    for(let i = 0; i < wordArray.length; i++){
+        if(vowels.includes(wordArray[i])){
+            vowelCount++;
+        }
+    }
+    //return the final vowel count
+    return vowelCount;
+};
