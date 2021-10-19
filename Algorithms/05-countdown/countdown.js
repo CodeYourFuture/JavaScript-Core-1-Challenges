a8f273cc9baba8459b89782a7acff54c5c034560
Sinead <77743497+Sinead-CYF@users.noreply.github.com>
2021-10-19 23:13:43 +0100

    Algorithm 5 complete

--- Algorithms/05-countdown/countdown.js
+++ Algorithms/05-countdown/countdown.js
@@ -1,4 +1,8 @@
 // Write code to print all numbers from `num` down to 1
 // You may assume `num` will be a positive number
 
-var countdown = function(num) {};
+var countdown = function(num) {
+    for (let i = 0; i <= num; i--){
+        console.log(i);
+    }
+};
