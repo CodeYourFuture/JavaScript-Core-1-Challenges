902aa77a44d1df8b9f21baaf9ddbd602e355b7b8
Sinead <77743497+Sinead-CYF@users.noreply.github.com>
2021-10-19 23:51:55 +0100

    Algorithm 9 complete

--- Algorithms/09-vowel-count/vowel-count.js
+++ Algorithms/09-vowel-count/vowel-count.js
@@ -1,3 +1,6 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function(str) {
+    var count = str.match(/[aeiou]/gi); //looks to match all vowels
+    return count === null ? 0 : count.length; //if string is empty return 0 else return count
+};
