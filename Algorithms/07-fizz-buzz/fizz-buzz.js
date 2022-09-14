ad21d39290eb46aff026599b38943945bb45370a
Baz Murphy <bazmurphy@gmail.com>
2022-09-14 12:26:15 +0100

    completed: algorithms, completed: challenges

--- Algorithms/07-fizz-buzz/fizz-buzz.js
+++ Algorithms/07-fizz-buzz/fizz-buzz.js
@@ -4,4 +4,16 @@
 // If a number is evenly divisible by both 3 AND 5, print "Fizz Buzz"
 // If a number is not divisible by 3 or 5, print the number
 
-var fizzBuzz = function(arr) {};
+var fizzBuzz = function(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] % 3 === 0 && arr[i] % 5 === 0) {
+      console.log("Fizz Buzz");
+    } else if (arr[i] % 3 === 0) {
+      console.log("Fizz");
+    } else if (arr[i] % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(arr[i]);
+    }
+  }
+};
--- Algorithms/09-vowel-count/vowel-count.js
+++ Algorithms/09-vowel-count/vowel-count.js
@@ -1,3 +1,12 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function(str) {
+  let numberOfVowels = 0;
+  for (let i = 0; i < str.length; i++) {
+    if ("aeiou".indexOf(str[i].toLowerCase()) > -1) {
+      numberOfVowels++;
+    }
+  }
+  return numberOfVowels;
+  // return str.split("").filter((element) => "aieou".indexOf(element.toLowerCase()) > -1).length;
+};
