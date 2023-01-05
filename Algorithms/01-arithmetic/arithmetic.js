f24cddbe8e62d6fe1edc6544f6418ba3ef8759e4
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-01-05 18:37:11 +0000

    Algorithms/01-arithmetic completed!

--- Algorithms/01-arithmetic/arithmetic.js
+++ Algorithms/01-arithmetic/arithmetic.js
@@ -1,9 +1,17 @@
 // Fill in the body of each function with the code required
 
-var add = function(num1, num2) {};
+var add = function(num1, num2) {
+    return num1 + num2;
+};
 
-var subtract = function(num1, num2) {};
+var subtract = function(num1, num2) {
+    return num1 - num2;
+};
 
-var multiply = function(num1, num2) {};
+var multiply = function(num1, num2) {
+    return num1 * num2;
+};
 
-var divide = function(num1, num2) {};
+var divide = function(num1, num2) {
+    return num1 / num2;
+};
