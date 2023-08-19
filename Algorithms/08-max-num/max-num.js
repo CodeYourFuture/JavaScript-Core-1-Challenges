ea7f990a79b5ff36dc2d2230461f1a2f3b321b59
Afsha10 <unityofbeing1@gmail.com>
2023-08-19 21:16:46 +0100

    Completed and tested 08-max-num

--- Algorithms/08-max-num/max-num.js
+++ Algorithms/08-max-num/max-num.js
@@ -1,3 +1,28 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var array = [3, 1, 17, 5, 6];
+
+var maxNum = function(arr) {
+    let biggestNumber = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > biggestNumber) {
+            biggestNumber = arr[i];
+        }
+    }
+    return biggestNumber;
+};
+
+console.log (maxNum(array));
+
+var maxNumForOfLoop = function(arr) {
+    let biggestNumber = arr[0];
+    for (const element of arr) {
+        if (element > biggestNumber)  {
+            biggestNumber = element;
+        }
+    }
+    return biggestNumber;
+}
+
+console.log (maxNumForOfLoop(array));
+
